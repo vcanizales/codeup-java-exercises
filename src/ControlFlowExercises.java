d0d58d571ea1260bf75d1a09e94faecf23eb0eb3
Valeria Canizales <vcanizales93@gmail.com>
2022-10-12 16:43:37 -0500

    Control flow exercises #1b part 2

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,9 +1,9 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 0;
+        int i = 100;
         do {
             System.out.println(i);
-            i = i + 2;
-        } while (i <= 100);
+            i = i - 5;
+        } while (i >= -10);
     }
 }
