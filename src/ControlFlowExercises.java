556265412d23c969acf5347aa5df2ad90ef0caef
Valeria Canizales <vcanizales93@gmail.com>
2022-10-12 21:47:27 -0500

    Control flow exercises #1c

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,9 +1,24 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        long i = 2;
-        do {
+//        int i = 100;
+//        do {
+//            System.out.println(i);
+//            i = i -5;
+//        } while (i >= -10 && i <= 100);
+
+//        for(int i = 100; i >= -10 && i <= 100; i-=5){
+//            System.out.println(i);
+//        }
+
+
+//        long i = 2;
+//        do {
+//            System.out.println(i);
+//            i = i*i;
+//        } while (i <= 1000000);
+
+        for(long i = 2; i <= 1000000; i*=i){
             System.out.println(i);
-            i = i*i;
-        } while (i <= 1000000);
+        }
     }
 }
