7b09fd8de9c03f93e16be0e14429bc74fecc4c4a
Valeria Canizales <vcanizales93@gmail.com>
2022-10-13 09:13:40 -0500

    Control flow exercises 2b

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -21,11 +21,17 @@ public class ControlFlowExercises {
 //            System.out.println(i);
 //        }
 
+//        for(int i = 1; i <= 100; i++){
+//            System.out.println(i);
+//        }
+//
         for(int i = 1; i <= 100; i++){
-            System.out.println(i);
+            if((i % 3) == 0) {
+                System.out.println("Fizz");
+            } else {
+                System.out.println(i);
+            }
         }
-
-
 
     }
 }
