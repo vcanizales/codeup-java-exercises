88e7a7d3ff1f8328acc85381e32189034f232480
Valeria Canizales <vcanizales93@gmail.com>
2022-10-14 16:00:51 -0500

    Methods Exercises 1 complete

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,22 +1,27 @@
 public class MethodsExercises {
-    public static int addition(){
-        return 1+1;
+
+    public static int addition(int x, int y){
+        return x+y;
     }
 
-    public static int subtraction(){
-        return 2-1;
+    public static int subtraction(int x, int y){
+        return x-y;
     }
 
-    public static int multiplication(){
-        return 2*1;
+    public static int multiplication(int x, int y){
+        return x*y;
     }
 
-    public static int division() {
-        return 10 / 5;
+    public static int division(int x, int y) {
+        return x/y;
+    }
+
+    public static int modulus(int x, int y) {
+        return x%y;
     }
 
 
     public static void main(String[] args) {
-        System.out.println(division());
+        System.out.println(division(5,0));
     }
 }
