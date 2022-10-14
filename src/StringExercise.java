5fe051b03426c9c62c175fb1d9725c0547eb36cd
Valeria Canizales <vcanizales93@gmail.com>
2022-10-13 21:33:52 -0500

    string exercise

---
+++ src/StringExercise.java
@@ -0,0 +1,10 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        String song = "We don't need no education";
+        song = song + "\nWe don't need no thought control";
+        System.out.println(song);
+
+        String msg = 'Check "this" out!';
+        msg = msg + 's inside of "s!';
+    }
+}
