6eea72ba0ce291f1f337cbd896d443da24077b48
Valeria Canizales <vcanizales93@gmail.com>
2022-10-14 23:40:46 -0500

    Methods Exercise 3 Starting on Factorial, not completed

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -2,24 +2,24 @@ import java.util.Scanner;
 
 public class MethodsExercises {
 
-    public static int addition(int x, int y){
-        return x+y;
+    public static int addition(int x, int y) {
+        return x + y;
     }
 
-    public static int subtraction(int x, int y){
-        return x-y;
+    public static int subtraction(int x, int y) {
+        return x - y;
     }
 
-    public static int multiplication(int x, int y){
-        return x*y;
+    public static int multiplication(int x, int y) {
+        return x * y;
     }
 
     public static int division(int x, int y) {
-        return x/y;
+        return x / y;
     }
 
     public static int modulus(int x, int y) {
-        return x%y;
+        return x % y;
     }
 
     public static int getInteger(int min, int max) {
@@ -35,7 +35,20 @@ public class MethodsExercises {
         }
     }
 
+    public static int factorial(int){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int i, fact = 1;
+        i = sc.nextInt();
+
+        for (i = 1; i <= fact; i++) {
+            return fact = fact * i;
+        }
+        System.out.println("Factorial of " + i + "is " + fact);
+    }
+
+
     public static void main(String[] args) {
-        getInteger(1, 10);
+        factorial();
     }
 }
