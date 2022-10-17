458c6c8acd992c3cf88a6d7ae79336cb62c03b91
Valeria Canizales <vcanizales93@gmail.com>
2022-10-17 09:28:26 -0500

    Methods Exercise 2

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class MethodsExercises {
@@ -22,33 +23,78 @@ public class MethodsExercises {
         return x % y;
     }
 
+//********************************************************************//
+// Factorial //
+
     public static int getInteger(int min, int max) {
         Scanner sc = new Scanner(System.in);
         System.out.printf("Enter a number between %d and %d:%n", min, max);
         int value = sc.nextInt();
 
         if (value <= 10 && value >= 1) {
-            return getInteger(min, max);
+            System.out.println("GOOD JOB!");;
         } else {
-            System.out.println("NOPE THAT'S NOT ETT!");
-            return value;
+            System.out.println("NOPE THAT'S NOT IT!");
         }
+        return value;
     }
-
-    public static int factorial(int){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number: ");
-        int i, fact = 1;
-        i = sc.nextInt();
-
-        for (i = 1; i <= fact; i++) {
-            return fact = fact * i;
-        }
-        System.out.println("Factorial of " + i + "is " + fact);
-    }
-
 
     public static void main(String[] args) {
-        factorial();
+        int userInput = getInteger(1, 10);
     }
-}
+
+//    public static int factorial(int num) {
+//        for (int i = 1; i <= num; i++) {
+//            return factorial(num) = num * i;
+//
+//        }
+//    }
+
+//        if (num >= 1)
+//            return num * factorial(num - 1);
+//        else
+//            return 1;
+
+
+//        int i = sc.nextInt();
+//        int fact = 1;
+//
+//
+//        for (i = 1; i <= fact; i++) {
+//        }
+//        return fact = fact * i;
+//        System.out.println("Factorial of " + i + "is " + fact);
+
+
+//    public static void main(String[] args) {
+//        do {
+//            System.out.println(getFactorial());
+//            System.out.println("Would you like to enter another number?: (yes/no)");
+//            Scanner sc = new Scanner(System.in);
+//            String choice = sc.next();
+//        }
+//        while (choice.equalsIgnoreCase("yes"));
+
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter a number: ");
+//        int num = sc.nextInt();
+//
+//        for (int i=0; i <= 10; i++){
+//            System.out.printf("%d! = %d x %d\t\t\t= %d%n", num,num,i,num*i);
+////            System.out.println(num +"! = " + num + " x " + i + "=" + (num*i));
+    }
+
+
+//    Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter a number between 1-10: ");
+//    int value = sc.nextInt();
+//    int fact = 1;
+//
+//        for (int i = 1; i > 0; i--) {
+//        System.out.println(i);
+//        fact = fact * 1;
+//    }
+//        return fact;
+//}
+
